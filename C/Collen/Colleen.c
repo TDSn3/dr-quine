1a9f8198121f40268752476ea1cb925252300c17
Thomas Da Silva <thomas-78000@hotmail.fr>
2025-01-03 10:41:43 +0100

    maj

---
+++ C/Collen/Colleen.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void p(char *s){printf(s,10,10,10,10,9,10,10,34,s,34,10,10,9,10);};
+int main(){
+/*
+	1
+*/
+char *s ="#include <stdio.h>%cvoid p(char *s){printf(s,10,10,10,10,9,10,10,34,s,34,10,10,9,10);};%cint main(){%c/*%c%c1%c*/%cchar *s =%c%s%c; p(s);}%c/*%c%c2%c*/"; p(s);}
+/*
+	2
+*/
