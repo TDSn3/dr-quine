7152fd1fdcf692c5024debfef3a35a6b37ed79b9
Thomas Da Silva <thomas-78000@hotmail.fr>
2025-01-03 09:24:18 +0100

    comment and FT update

--- C/Collen/colleen.c
+++ C/Collen/colleen.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
-void p(char *s){printf(s,10,47,47,34,s,34,47,47);};int main(){/*1*/char *s ="#include <stdio.h>%cvoid p(char *s){printf(s,10,47,47,34,s,34,47,47);};int main(){%c*1*%cchar *s =%c%s%c; p(s);}%c%c2"; p(s);}//2
+void p(char *s){printf(s,10,10,10,10,9,10,10,34,s,34,10,10,9,10);};
+int main(){
+/*
+	1
+*/
+char *s ="#include <stdio.h>%cvoid p(char *s){printf(s,10,47,47,34,s,34);};%cint main(){%c/*%c%c1%c*/%cchar *s =%c%s%c; p(s);}%c/*%c%c2%c*/"; p(s);}
+/*
+	2
+*/
--- C/Grace/grace.c
+++ C/Grace/grace.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#define A "#include <stdio.h>%c#define A %c%s%c%c#define B %cGrace_kid.c%c%c#define C(A,B) int main(){FILE *d=fopen(B,%cw%c);fprintf(d, A, 10, 34, A, 34, 10, 34, 34, 10, 34, 34, 10, 10, 47, 47);}%c%cC(A,B)%c%c"
+#define A "#include <stdio.h>%c#define A %c%s%c%c#define B %cGrace_kid.c%c%c#define FT(A,B) int main(){FILE *d=fopen(B,%cw%c);fprintf(d, A, 10, 34, A, 34, 10, 34, 34, 10, 34, 34, 10, 10, 10, 9, 10, 10, 10);}%c%c/*%c%cC%c*/%c%cFT(A,B)"
 #define B "Grace_kid.c"
-#define C(A,B) int main(){FILE *d=fopen(B,"w");fprintf(d, A, 10, 34, A, 34, 10, 34, 34, 10, 34, 34, 10, 10, 47, 47);}
+#define FT(A,B) int main(){FILE *d=fopen(B,"w");fprintf(d, A, 10, 34, A, 34, 10, 34, 34, 10, 34, 34, 10, 10, 10, 9, 10, 10, 10);}
 
-C(A,B)//
+/*
+	C
+*/
+
+FT(A,B)
